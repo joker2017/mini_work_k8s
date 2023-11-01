92f2908888eaf3fe554e43caa89ec9aff533ff38
Andrey Shcherbina <depsemt2009@ya.ru>
2023-11-01 20:08:56 +0300

    unittest

--- account/app/app/urls.py
+++ account/app/app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     flagged_path('ACCOUNT_LIST_FLAG', 'account/list/', AccountList.as_view({'get': 'list'}), state=False),
     flagged_path('ACCOUNT_CREATE_FLAG', 'account/create/', AccountCreate.as_view({'post': 'create'}), state=False),
-    flagged_pathh('ACCOUNT_UPDATE_FLAG', 'account/update/<str:pk>/', AccountUpdate.as_view(), state=False),
-    flagged_pathh('ACCOUNT_DESTROY_FLAG', 'account/destroy/<str:pk>/', AccountDestroy.as_view(), state=False),
+    flagged_path('ACCOUNT_UPDATE_FLAG', 'account/update/<str:pk>/', AccountUpdate.as_view(), state=False),
+    flagged_path('ACCOUNT_DESTROY_FLAG', 'account/destroy/<str:pk>/', AccountDestroy.as_view(), state=False),
     flagged_path('ACCOUNT_DETAIL_FLAG', 'account/detail/<str:usernameid>/', AccountDetail.as_view(), state=False),
     
 
