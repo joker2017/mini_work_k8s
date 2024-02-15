b09f12cc445b38ba69348944a862f956ebd56817
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-15 23:35:59 +0300

    1

--- account/app/app/settings.py
+++ account/app/app/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = './app/app/vol/web/media'
-STATIC_ROOT = './app/app/vol/web/static'
+STATIC_ROOT = './app/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
