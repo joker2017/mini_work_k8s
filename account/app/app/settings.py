236ff9e726d4c50ae5d77a504e9d2d574b8b64db
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-17 08:44:36 +0300

    1

--- account/app/app/settings.py
+++ account/app/app/settings.py
@@ -170,3 +170,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print(f"BASE_DIR: {BASE_DIR}")
+print(f"STATIC_ROOT: {STATIC_ROOT}")
