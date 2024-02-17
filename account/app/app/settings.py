038416bfcc78d6c459a349bf77cf70ad1a5461e5
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-17 07:50:40 +0300

    1

--- account/app/app/settings.py
+++ account/app/app/settings.py
@@ -159,11 +159,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+#MEDIA_ROOT = '/vol/web/media'
+#STATIC_ROOT = '/vol/web/static'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
