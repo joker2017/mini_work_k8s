f6de937e0551892072d212055d3a1a36037b6ac9
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-18 22:40:11 +0300

    1

--- account/app/app/settings.py
+++ account/app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jdk9%)1!9grg$5q3@*v5*%4vh5b$_ns@(#g9&o@#mb-df9!8z0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '158.160.146.151', 'localhost']
+ALLOWED_HOSTS = ['*', 'localhost']
 
 
 # Application definition
@@ -158,14 +158,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 #MEDIA_ROOT = '/vol/web/media'
 #STATIC_ROOT = '/vol/web/static'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
