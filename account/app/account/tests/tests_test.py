c7f10556fa7fe1e829acc731403c6b436cd4d8f9
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-20 01:48:52 +0300

    1

--- account/app/account/tests/tests_test.py
+++ account/app/account/tests/tests_test.py
@@ -8,33 +8,29 @@ from account.serializers import AccountSerializer, AccountSerializerRegistr
 # Предположим, что у вас есть такие модули
 from account.services import create_account_number
 
-# Имитация модели Account для тестов
+# Мок модели Account
 mock_account = MagicMock()
 
-# Тесты для API, использующие мокирование вместо реальной базы данных
-class AccountAPITestCase(BaseTestCase):
+class AccountAPITestCase(APITestCase):
     @patch('account.models.Account.objects.all')
     def test_account_list(self, mock_all):
         mock_all.return_value = [mock_account]
-        client = APIClient()
-        response = client.get('/account/list/')
+        response = self.client.get('/account/list/')
         self.assertEqual(response.status_code, 200)
         mock_all.assert_called_once()
 
-    @patch('account.services.create_account_number')
+    @patch('account.views.create_account_number')
     @patch('account.serializers.AccountSerializerRegistr.save')
     def test_account_create(self, mock_save, mock_create_account_number):
         mock_create_account_number.return_value = '1234567890'
         mock_save.return_value = mock_account
-        client = APIClient()
         data = {'name': 'Test Account', 'balance': 1000}
-        response = client.post('/account/create/', data, format='json')
+        response = self.client.post('/account/create/', data, format='json')
         self.assertEqual(response.status_code, 201)
         mock_create_account_number.assert_called_once()
         mock_save.assert_called_once()
 
-# Тесты для сервиса создания номера аккаунта
-class CreateAccountNumberTestCase(BaseTestCase):
+class CreateAccountNumberTestCase(SimpleTestCase):
     @patch('account.models.Account.objects.filter')
     def test_create_account_number_success(self, mock_filter):
         mock_filter.return_value.exists.return_value = False
@@ -43,14 +39,15 @@ class CreateAccountNumberTestCase(BaseTestCase):
         self.assertTrue(account_number.isdigit())
         mock_filter.assert_called_once_with(id=account_number)
 
-# Тесты для сериализаторов
-class AccountSerializerTestCase(BaseTestCase):
+class AccountSerializerTestCase(SimpleTestCase):
+    @patch('account.models.Account.save', MagicMock())
     def test_account_serializer_fields(self):
         account_data = {'name': 'Test Account', 'balance': 1000}
         serializer = AccountSerializer(data=account_data)
         self.assertTrue(serializer.is_valid())
-        self.assertEqual(set(serializer.validated_data.keys()), set(['name', 'balance']))
+        self.assertEqual(set(serializer.validated_data.keys()), {'name', 'balance'})
 
+    @patch('account.models.Account.save', MagicMock())
     def test_account_serializer_registr_save(self):
         account_registr_data = {'name': 'Test Account', 'balance': 1000}
         serializer = AccountSerializerRegistr(data=account_registr_data)
