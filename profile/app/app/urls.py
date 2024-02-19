2a73d8b728f01c00daaa6d28a4f1a7d88eff5981
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-19 03:51:35 +0300

    1

--- profile/app/app/urls.py
+++ profile/app/app/urls.py
@@ -7,9 +7,9 @@ from flags.urls import flagged_path, flagged_paths, flagged_re_path, flagged_re_
 
 urlpatterns = [
 
-    flagged_path('SEARCHS_FLAG', 'usersearch/', UsersListView.as_view(), state=False),
+    flagged_path('SEARCHS_FLAG', 'profile/usersearch/', UsersListView.as_view(), state=False),
     flagged_path('USERS_CREATE_FLAG', 'profile/create/', UsersCreate.as_view({'post': 'create'}), state=False),
-    path('admin/', admin.site.urls),
+    path('profile/admin/', admin.site.urls),
     flagged_path('USERS_UPDATE_FLAG', 'profile/update/<str:pk>/', UsersUpdate.as_view(), state=False),
     flagged_path('USERS_DESTROY_FLAG', 'profile/destroy/<str:pk>/', UsersDestroy.as_view(), state=False),
     flagged_path('USERSDETAIL_FLAG', 'profile/detail/<str:id>/', UsersDetail.as_view(), state=False),
