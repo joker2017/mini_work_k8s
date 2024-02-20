29f3d8346eb96ab84d2efcb4cc4d996bd3c3e8b5
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-20 15:39:09 +0300

    1

--- account/app/account/tests/tests_test.py
+++ account/app/account/tests/tests_test.py
@@ -25,9 +25,9 @@ def test_create_account_number(mock_filter):
 
 
 
-
+from rest_framework import status
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 @pytest.fixture
 def mock_create_account_number(mocker):
@@ -37,7 +37,7 @@ def mock_create_account_number(mocker):
 def mock_account_serializer(mocker):
     serializer_mock = Mock()
     serializer_mock.is_valid.return_value = True
-    serializer_mock.save.return_value = None  # Замените на нужное значение
+    serializer_mock.save.return_value = None  # Замените на нужное значение, если необходимо
     serializer_mock.data = {
         'id': '12345678901234567890',
         'balance': '100.00',
@@ -48,18 +48,20 @@ def mock_account_serializer(mocker):
 def test_account_create(mock_create_account_number, mock_account_serializer):
     from account.app.account.views import AccountCreate  # Импорт здесь, чтобы избежать циклических зависимостей
 
-    view = AccountCreate()
-    request = Mock(data={
-        'balance': '100.00',
-        'usernameid': 'test_user_id'
-    })
-    view.request = request
-    response = view.create(request)
+    with patch.object(AccountCreate, 'get_serializer_context', return_value={'request': Mock(), 'view': Mock()}):
+        view = AccountCreate()
+        request = Mock(data={
+            'balance': '100.00',
+            'usernameid': 'test_user_id'
+        })
+        request._request = Mock()  # Эмуляция HttpRequest объекта, если требуется
+        view.request = request
+        view.kwargs = {}  # Эмуляция kwargs, если ваш метод их использует
+        response = view.create(request)
 
-    assert response.status_code == status.HTTP_201_CREATED
-    mock_create_account_number.assert_called_once()
-    mock_account_serializer().is_valid.assert_called_once_with(raise_exception=True)
-    assert response.data['id'] == '12345678901234567890'
-    assert response.data['balance'] == '100.00'
-    assert response.data['usernameid'] == 'test_user_id'
-
+        assert response.status_code == status.HTTP_201_CREATED
+        mock_create_account_number.assert_called_once()
+        mock_account_serializer().is_valid.assert_called_once_with(raise_exception=True)
+        assert response.data['id'] == '12345678901234567890'
+        assert response.data['balance'] == '100.00'
+        assert response.data['usernameid'] == 'test_user_id'
