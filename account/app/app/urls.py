982a932d39c35100dfabed00e2a4bdfcf6dfa1c5
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-20 22:55:16 +0300

    1

--- account/app/app/urls.py
+++ account/app/app/urls.py
@@ -9,11 +9,11 @@ from flags.urls import flagged_path, flagged_paths, flagged_re_path, flagged_re_
 
 urlpatterns = [
     path('account/admin/', admin.site.urls),
-    flagged_path('ACCOUNT_LIST_FLAG', 'account/list/', AccountList.as_view({'get': 'list'}), name='account-list', state=False),
-    flagged_path('ACCOUNT_CREATE_FLAG', 'account/create/', AccountCreate.as_view({'post': 'create'}), name='account-create', state=False),
-    flagged_path('ACCOUNT_UPDATE_FLAG', 'account/update/<str:pk>/', AccountUpdate.as_view(), name='account-update', state=False),
-    flagged_path('ACCOUNT_DESTROY_FLAG', 'account/destroy/<str:pk>/', AccountDestroy.as_view(), name='account-destroy', state=False),
-    flagged_path('ACCOUNT_DETAIL_FLAG', 'account/detail/<str:usernameid>/', AccountDetail.as_view(), name='account-detail', state=False),
+    flagged_path('ACCOUNT_LIST_FLAG', 'account/list/', AccountList.as_view({'get': 'list'}), state=False),
+    flagged_path('ACCOUNT_CREATE_FLAG', 'account/create/', AccountCreate.as_view({'post': 'create'}), state=False),
+    flagged_path('ACCOUNT_UPDATE_FLAG', 'account/update/<str:pk>/', AccountUpdate.as_view(), state=False),
+    flagged_path('ACCOUNT_DESTROY_FLAG', 'account/destroy/<str:pk>/', AccountDestroy.as_view(), state=False),
+    flagged_path('ACCOUNT_DETAIL_FLAG', 'account/detail/<str:usernameid>/', AccountDetail.as_view(), state=False),
     
 
 ]
