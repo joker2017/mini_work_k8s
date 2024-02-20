5c8d2495eca3a49316e6cf56dad66336671bdb46
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-20 11:14:29 +0300

    1

--- account/app/account/tests/tests_test.py
+++ account/app/account/tests/tests_test.py
@@ -2,22 +2,15 @@
 import unittest
 from unittest.mock import patch
 from ..services import create_account_number
-
-
 import pytest
 from unittest.mock import patch
 
+@patch('account.app.account.models.Account.objects.filter')
+def test_create_account_number(mock_filter):
+    mock_filter.return_value.exists.return_value = False
 
+    account_number = create_account_number()
 
-def test_create_account_number():
-    with patch('account.app.account.models.Account.objects.filter') as mock_filter:
-        mock_filter.return_value.exists.return_value = False
-
-        account_number = create_account_number()
-
-        assert len(account_number) == 20
-        assert account_number.isdigit()
-        mock_filter.assert_called()
-
-
-
+    assert len(account_number) == 20
+    assert account_number.isdigit()
+    mock_filter.assert_called_once()  # Или используйте assert_called_once_with(), если вы хотите проверить вызов с конкретными аргументами
