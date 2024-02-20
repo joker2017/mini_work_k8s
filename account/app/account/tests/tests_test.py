759508a83ab69440477a1412164a19e8d7e51313
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-20 04:56:51 +0300

    1

--- account/app/account/tests/tests_test.py
+++ account/app/account/tests/tests_test.py
@@ -1,39 +1,23 @@
-
-#from unittest.mock import patch
-#from account.models import Account
-#from account.views import AccountCreate, AccountList
-
-
+# test_services.py
 import unittest
 from unittest.mock import patch
-#from myapp.models import Account  # Импорт модели Account из вашего приложения
-#from account.app.app.services import create_account_number  # Предполагается, что функция определена в myapp/functions.py
-from account.services import create_account_number
-class CreateAccountNumberTest(unittest.TestCase):
-    @patch('Account.objects.filter')
-    def test_create_account_number_unique(self, mock_filter):
-        # Настройка мока, чтобы возвращать пустой QuerySet, имитируя отсутствие совпадений в базе данных
+from services import create_account_number
+
+class TestCreateAccountNumber(unittest.TestCase):
+    @patch('services.Account.objects.filter')
+    def test_create_account_number(self, mock_filter):
+        # Настройка мока
         mock_filter.return_value.exists.return_value = False
 
+        # Вызов функции
         account_number = create_account_number()
 
-        # Проверяем, что функция возвращает строку длиной 20 символов
+        # Проверки
         self.assertEqual(len(account_number), 20)
-        # Проверяем, что функция generate_id вызывалась хотя бы один раз
+        self.assertTrue(account_number.isdigit())
+
+        # Убедитесь, что мок был вызван
         mock_filter.assert_called()
-
-    @patch('Account.objects.filter')
-    def test_create_account_number_retry_on_duplicate(self, mock_filter):
-        # Настройка мока, чтобы сначала возвращать True, имитируя существующий номер счета,
-        # а затем False, имитируя успешное создание уникального номера счета
-        mock_filter.side_effect = [True, False]
-
-        account_number = create_account_number()
-
-        # Проверяем, что функция возвращает строку длиной 20 символов
-        self.assertEqual(len(account_number), 20)
-        # Проверяем, что функция generate_id была вызвана дважды: один раз для существующего номера счета и один раз для уникального
-        self.assertEqual(mock_filter.call_count, 2)
 
 if __name__ == '__main__':
     unittest.main()
