8a84acc69d26864f463700f9ab06588d8b9c1133
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-21 09:07:34 +0300

    1

--- profile/app/user_profile/tests/tests_test.py
+++ profile/app/user_profile/tests/tests_test.py
@@ -17,19 +17,19 @@ def mock_user_instance():
 
 
 @pytest.fixture
-def account_instance(mock_user_instance):
+def users_instance(mock_user_instance):
     """Фикстура для создания мок экземпляра аккаунта."""
-    return Mock(spec=Account, id='12345678901234567890', balance=100.00, usernameid=mock_user_instance)
+    return Mock(full_names=ivan, username=test_user_id, email=ivan@ya.ru, password=1234567890)
 
 
 @pytest.fixture
-def mock_profile_serializer(account_instance):
+def mock_profile_serializer(users_instance):
     """Фикстура для создания мок сериализатора."""
     serializer_mock = Mock(spec=UsersSerializer)
-    serializer_mock.instance = account_instance
+    serializer_mock.instance = users_instance
     serializer_mock.is_valid.return_value = True
-    serializer_mock.save.return_value = account_instance
-    serializer_mock.data = {'id': account_instance.id, 'balance': '200.00', 'usernameid': account_instance.usernameid.id}
+    serializer_mock.save.return_value = users_instance
+    serializer_mock.data = {'full_names': 'ivan', 'username': 'test_user_id', 'email': 'ivan@ya.ru', 'password': '1234567890'}
     return serializer_mock
 
 
@@ -37,41 +37,48 @@ def mock_profile_serializer(account_instance):
 def test_create_profile_number(mock_filter):
     """Тест проверяет создание номера аккаунта."""
     mock_filter.return_value.exists.return_value = False
-    account_number = create_account_number()
-    assert len(account_number) == 20 and account_number.isdigit()
+    users_number = create_account_number()
+    assert len(users_number) == 20 and users_number.isdigit()
     mock_filter.assert_called()
 
 
-def test_profile_create_with_mocked_view(mock_account_serializer):
+
+    full_names = models.CharField(max_length=255)  #Full_names
+    username = models.CharField(max_length=255)  #username
+    email = models.EmailField(max_length=255)   #email
+    password = models.CharField(max_length=255)  # password
+
+
+def test_profile_create_with_mocked_view(mock_users_serializer):
     """Тест проверяет создание аккаунта с мокированным представлением."""
-    request = RequestFactory().post('/fake-url/', data={'balance': '100.00', 'usernameid': 'test_user_id'})
+    request = RequestFactory().post('/fake-url/', data={'full_names': 'ivan', 'username': 'test_user_id', 'email': 'ivan@ya.ru', 'password': '1234567890'})
     with patch.object(UsersCreate, 'create', return_value=Response(
-            mock_account_serializer.data, status=status.HTTP_201_CREATED)) as mock_method:
+            mock_users_serializer.data, status=status.HTTP_201_CREATED)) as mock_method:
         response = UsersCreate.as_view({'post': 'create'})(request)
         assert mock_method.called
         assert response.status_code == status.HTTP_201_CREATED
-        assert response.data == mock_account_serializer.data
+        assert response.data == mock_users_serializer.data
 
 
-def test_account_update_with_mocked_response(mock_account_serializer):
+def test_users_update_with_mocked_response(mock_users_serializer):
     """Тест проверяет обновление аккаунта с мокированным ответом."""
     client = APIClient()
     with patch('profile.app.user_profile.views.ProfileUpdate', return_value=Response(
-            mock_account_serializer.data, status=status.HTTP_200_OK)) as mocked_put:
+            mock_users_serializer.data, status=status.HTTP_200_OK)) as mocked_put:
         response = mocked_put(Mock(data={'balance': '200.00'}))
         assert mocked_put.called
         assert response.status_code == status.HTTP_200_OK
-        assert response.data == mock_account_serializer.data
+        assert response.data == mock_users_serializer.data
 
 
-def test_account_destroy_with_mocked_response(account_instance):
+def test_users_destroy_with_mocked_response(users_instance):
     """Тест проверяет удаление аккаунта с мокированным ответом."""
-    with patch('profile.app.user_profile.views.ProfileDestroy.get_object', return_value=account_instance), \
+    with patch('profile.app.user_profile.views.ProfileDestroy.get_object', return_value=users_instance), \
          patch('profile.app.user_profile.views.ProfileDestroy.perform_destroy', return_value=None):
         view = UsersDestroy()
         request = Mock()
-        view.setup(request, pk=account_instance.id)
-        response = view.delete(request, pk=account_instance.id)
+        view.setup(request, pk=users_instance.id)
+        response = view.delete(request, pk=users_instance.id)
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
