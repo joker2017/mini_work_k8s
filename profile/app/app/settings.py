c97d502a0b9053060ac454130e1b6930968bd6cd
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-21 05:43:20 +0300

    1

--- profile/app/app/settings.py
+++ profile/app/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profile.app.user_profile',
+    'user_profile',
     'flags',
     'rest_framework',
     'django_filters',
--- profile/app/user_profile/models.py
+++ profile/app/user_profile/models.py
@@ -32,6 +32,8 @@ class Users(models.Model):
     def __str__(self):
         return f'{self.full_names} {self.username}'
 
+    class Meta:
+        app_label = 'user_profile'
 
 
 
