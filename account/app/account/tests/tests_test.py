063444c0db82a6e3dec7187d8f1e3c82c4385f8e
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-22 02:32:34 +0300

    1

--- account/app/account/tests/tests_test.py
+++ account/app/account/tests/tests_test.py
@@ -1,30 +1,30 @@
 import pytest
-from unittest.mock import Mock, patch
+from hashlib import sha256
+from unittest.mock import Mock, patch, MagicMock
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.test import APIClient
 from django.test import RequestFactory
-from account.app.account.models import Account, Users
-from account.app.account.views import AccountCreate, AccountUpdate, AccountDestroy
-from account.app.account.serializers import AccountSerializerRegistr, AccountSerializer
+from django.db import IntegrityError
+from account.app.account.views import AccountCreate, AccountDestroy
+from account.app.account.serializers import AccountSerializer
 from account.app.account.services import create_account_number
+from account.app.account.models import Users, Account
 
 
 @pytest.fixture
 def mock_user_instance():
-    """Фикстура для создания мок экземпляра пользователя."""
+    """Fixture for creating a mock user instance."""
     return Mock(spec=Users, id='test_user_id')
-
 
 @pytest.fixture
 def account_instance(mock_user_instance):
-    """Фикстура для создания мок экземпляра аккаунта."""
+    """Fixture for creating a mock account instance."""
     return Mock(spec=Account, id='12345678901234567890', balance=100.00, usernameid=mock_user_instance)
-
 
 @pytest.fixture
 def mock_account_serializer(account_instance):
-    """Фикстура для создания мок сериализатора."""
+    """Fixture for creating a mock serializer."""
     serializer_mock = Mock(spec=AccountSerializer)
     serializer_mock.instance = account_instance
     serializer_mock.is_valid.return_value = True
@@ -32,18 +32,16 @@ def mock_account_serializer(account_instance):
     serializer_mock.data = {'id': account_instance.id, 'balance': '200.00', 'usernameid': account_instance.usernameid.id}
     return serializer_mock
 
-
 @patch('account.app.account.models.Account.objects.filter')
 def test_create_account_number(mock_filter):
-    """Тест проверяет создание номера аккаунта."""
+    """Test checks the creation of an account number."""
     mock_filter.return_value.exists.return_value = False
     account_number = create_account_number()
     assert len(account_number) == 20 and account_number.isdigit()
     mock_filter.assert_called()
 
-
 def test_account_create_with_mocked_view(mock_account_serializer):
-    """Тест проверяет создание аккаунта с мокированным представлением."""
+    """Test checks account creation with a mocked view."""
     request = RequestFactory().post('/fake-url/', data={'balance': '100.00', 'usernameid': 'test_user_id'})
     with patch.object(AccountCreate, 'create', return_value=Response(
             mock_account_serializer.data, status=status.HTTP_201_CREATED)) as mock_method:
@@ -52,9 +50,8 @@ def test_account_create_with_mocked_view(mock_account_serializer):
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data == mock_account_serializer.data
 
-
 def test_account_update_with_mocked_response(mock_account_serializer):
-    """Тест проверяет обновление аккаунта с мокированным ответом."""
+    """Test checks account update with a mocked response."""
     client = APIClient()
     with patch('account.app.account.views.AccountUpdate', return_value=Response(
             mock_account_serializer.data, status=status.HTTP_200_OK)) as mocked_put:
@@ -63,9 +60,8 @@ def test_account_update_with_mocked_response(mock_account_serializer):
         assert response.status_code == status.HTTP_200_OK
         assert response.data == mock_account_serializer.data
 
-
 def test_account_destroy_with_mocked_response(account_instance):
-    """Тест проверяет удаление аккаунта с мокированным ответом."""
+    """Test checks account deletion with a mocked response."""
     with patch('account.app.account.views.AccountDestroy.get_object', return_value=account_instance), \
          patch('account.app.account.views.AccountDestroy.perform_destroy', return_value=None):
         view = AccountDestroy()
@@ -74,35 +70,9 @@ def test_account_destroy_with_mocked_response(account_instance):
         response = view.delete(request, pk=account_instance.id)
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import hashlib
-from hashlib import sha256
-import pytest
-from unittest.mock import patch, MagicMock
-from account.app.account.models import Users, Account
-from django.db import IntegrityError
-
 @pytest.fixture
 def user_data():
+    """Returns a dictionary with user data."""
     return {
         'email': 'test@example.com',
         'full_names': 'Test User',
@@ -110,8 +80,10 @@ def user_data():
         'password': 'password123',
         'username': 'test_user'
     }
+
 @pytest.fixture
 def account_data():
+    """Returns a dictionary with account data."""
     return {
         'id': 'test_account_id',
         'balance': 100.00,
@@ -119,11 +91,13 @@ def account_data():
     }
 
 def test_user_creation(user_data):
+    """Tests user creation."""
     with patch('account.app.account.models.Users.objects.create') as mock_create:
         Users.objects.create(**user_data)
         mock_create.assert_called_once_with(**user_data)
 
 def test_unique_username_constraint(user_data):
+    """Tests that a username must be unique."""
     with patch('account.app.account.models.Users.objects.create') as mock_create:
         mock_create.side_effect = [None, IntegrityError()]
         Users.objects.create(**user_data)  # Creating the first user
@@ -131,45 +105,38 @@ def test_unique_username_constraint(user_data):
             Users.objects.create(**user_data)  # Attempt to create a user with the same username
 
 def test_account_creation(account_data):
+    """Tests account creation."""
     with patch('account.app.account.models.Account.objects.create') as mock_create:
         Account.objects.create(**account_data)
         mock_create.assert_called_once_with(**account_data)
 
 def test_account_balance_default():
-    # Mocking the Account.objects.create method to return a MagicMock object
+    """Tests that the default balance is correctly set."""
     with patch('account.app.account.models.Account.objects.create') as mock_create:
-        # Creating a MagicMock instance to mimic an Account object
         mock_account_instance = MagicMock()
-        mock_account_instance.balance = 0  # Set the balance attribute of the MagicMock object to 0
-        mock_create.return_value = mock_account_instance  # Make the mocked create method return the MagicMock object
+        mock_account_instance.balance = 0
+        mock_create.return_value = mock_account_instance
         account = Account.objects.create()
         assert account.balance == 0
 
 def test_account_usernameid_null():
-    # Mocking the Account.objects.create method to return a MagicMock object
+    """Tests that usernameid is null by default."""
     with patch('account.app.account.models.Account.objects.create') as mock_create:
-        # Creating a MagicMock instance to mimic an Account object
         mock_account_instance = MagicMock()
-        mock_account_instance.usernameid = None  # Set the usernameid attribute of the MagicMock object to None
-        mock_create.return_value = mock_account_instance  # Make the mocked create method return the MagicMock object
+        mock_account_instance.usernameid = None
+        mock_create.return_value = mock_account_instance
         account = Account.objects.create()
         assert account.usernameid is None
 
-
 def test_user_password_hashing(user_data):
+    """Tests password hashing for a user."""
     with patch.object(Users, 'save', autospec=True) as mock_save:
         user = Users(**user_data)
-
-        # Хешируем пароль перед сохранением
+        # Hashing the password before saving
         user.password = sha256(user.password.encode('utf-8')).hexdigest()
-
         user.save()
         mock_save.assert_called_once()
-
+        # Verifying that the hashed password matches the expected hash
         saved_password = user.password
-
-        # Хешируем ожидаемый пароль
         expected_hashed_password = sha256(user_data['password'].encode('utf-8')).hexdigest()
-
-        # Сравниваем хешированные пароли
         assert saved_password == expected_hashed_password
