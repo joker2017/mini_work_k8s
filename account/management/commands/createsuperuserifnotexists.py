3d0bd40fef650021c297d87215f69087f4f09be8
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-22 12:49:54 +0300

    1

--- account/management/commands/createsuperuserifnotexists.py
+++ account/management/commands/createsuperuserifnotexists.py
@@ -1,20 +1,16 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Creates a superuser if not exists'
+    help = 'Create superuser if not exists'
 
-    def add_arguments(self, parser):
-        parser.add_argument('username', type=str, help='Superuser username')
-        parser.add_argument('email', type=str, help='Superuser email')
-        parser.add_argument('password', type=str, help='Superuser password')
-
-    def handle(self, *args, **options):
-        username = options['username']
-        email = options['email']
-        password = options['password']
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        username = kwargs.get('username')
+        email = kwargs.get('email')
+        password = kwargs.get('password')
         if not User.objects.filter(username=username).exists():
-            print(f"Creating superuser {username}")
             User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
         else:
-            print(f"Superuser {username} already exists.")
+            self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
