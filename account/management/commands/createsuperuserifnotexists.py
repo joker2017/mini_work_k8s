6c0a5b03b192f0490f057560bb23f525eac07a0b
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-22 12:55:36 +0300

    1

--- account/management/commands/createsuperuserifnotexists.py
+++ account/management/commands/createsuperuserifnotexists.py
@@ -1,5 +1,9 @@
+# management/commands/createsuperuserifnotexists.py
+
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+
 
 class Command(BaseCommand):
     help = 'Create superuser if not exists'
@@ -9,8 +13,9 @@ class Command(BaseCommand):
         username = kwargs.get('username')
         email = kwargs.get('email')
         password = kwargs.get('password')
+
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
+            call_command('createsuperuser', username=username, email=email, interactive=False)
             self.stdout.write(self.style.SUCCESS(f'Superuser {username} created'))
         else:
             self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
