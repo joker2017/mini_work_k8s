cf0b5855d5f6c759a0d50d6e44ff186fe0681b5f
Andrey Shcherbina <depsemt2009@ya.ru>
2024-02-23 05:10:32 +0300

    1

--- profile/app/user_profile/views.py
+++ profile/app/user_profile/views.py
@@ -36,17 +36,46 @@ class UsersDetail(generics.RetrieveAPIView):
 
 
 # View for deleting a user
-class UsersDestroy(generics.DestroyAPIView, generics.RetrieveAPIView):
-    queryset = Users.objects.all()  # Queryset that includes all users
-    serializer_class = UsersSerializer  # Serializer class for user data
-    lookup_fields = 'id'  # Field to look up user
+#class UsersDestroy(generics.DestroyAPIView, generics.RetrieveAPIView):
+#    queryset = Users.objects.all()  # Queryset that includes all users
+#    serializer_class = UsersSerializer  # Serializer class for user data
+#    lookup_fields = 'id'  # Field to look up user
 
 
-    def destroy(self, request, *args, **kwargs):
-          instance = self.get_object()
-          if instance.accounts.exists():
-             # Связанные аккаунты существуют, возвращаем ошибку
-             return Response({"detail": "Невозможно удалить пользователя с привязанными аккаунтами"}, status=status.HTTP_403_FORBIDDEN)
+#    def destroy(self, request, *args, **kwargs):
+#          instance = self.get_object()
+#          if instance.accounts.exists():
+#             # Связанные аккаунты существуют, возвращаем ошибку
+#             return Response({"detail": "Невозможно удалить пользователя с привязанными аккаунтами"}, status=status.HTTP_403_FORBIDDEN)
 
           # Если связанных аккаунтов нет, удаляем пользователя
-          return super().destroy(request, *args, **kwargs)
+#          return super().destroy(request, *args, **kwargs)
+
+
+class UsersDetail(generics.ListAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    lookup_fields = 'id'
+
+    def get_queryset(self):
+        return self.queryset.filter(
+            id=self.kwargs['id'])
+
+
+class UsersDestroy(generics.DestroyAPIView, generics.RetrieveAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+    lookup_fields = 'id'
+
+    def get_queryset(self):
+        queryset = Users.objects.filter(id=self.kwargs['pk'])
+        return queryset
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        try:
+            self.perform_destroy(instance)
+        except ProtectedError as e:
+            return Response("Нельзя удалить клиента привязаными счетами", status=status.HTTP_403_FORBIDDEN)
+        return Response(status=status.HTTP_204_NO_CONTENT)
